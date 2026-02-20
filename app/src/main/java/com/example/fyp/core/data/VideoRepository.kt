24983cfa2e90f044b0ac6564a805ccbc08e13971
Morgan <morper04@gmail.com>
2026-02-20 14:40:26 +0000

    Created ViewModel structure for recorded videos that uses Room DB

--- app/src/main/java/com/example/fyp/core/data/VideoRepository.kt
+++ app/src/main/java/com/example/fyp/core/data/VideoRepository.kt
@@ -1,2 +1,21 @@
 package com.example.fyp.core.data
 
+
+import com.example.fyp.core.database.dao.PublishedVideoDao
+import com.example.fyp.core.database.dao.SubscribedVideoDao
+import com.example.fyp.core.database.entities.PublishedVideoEntity
+import com.example.fyp.core.database.entities.SubscribedVideoEntity
+import kotlinx.coroutines.flow.Flow
+
+class VideoRepository(
+    private val publishedVideoDao: PublishedVideoDao,
+    private val subscribedVideoDao: SubscribedVideoDao
+) {
+    // Get streams of data that update automatically
+    val myVideos: Flow<List<PublishedVideoEntity>> = publishedVideoDao.getMyPublishedVideos()
+    val subscribedVideos: Flow<List<SubscribedVideoEntity>> = subscribedVideoDao.getMySubscribedVideos()
+
+    suspend fun savePublishedVideo(video: PublishedVideoEntity) {
+        publishedVideoDao.insertVideo(video)
+    }
+}
